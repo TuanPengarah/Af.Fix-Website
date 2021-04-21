5597298124c1b85b2a06ab0d8f8bb9aec2959b3c
TuanPengarah <akidfikriazhar@gmail.com>
2021-04-21 23:42:50 +0800

    Initialize firebase hosting Initialize apps localization BM localization EN localization More improvement on 1st landing page Initialize Repair Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,61 @@
+import 'package:affix_web/config/app_localizations.dart';
 import 'package:affix_web/config/routes.dart';
 import 'package:affix_web/screen/homescreen/home.dart';
 import 'package:affix_web/screen/myrepairid.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'config/change_lang.dart';
 
-void main() {
+void main() async {
   setPathUrlStrategy();
-  runApp(MyApp());
+  AppLanguage appLanguage = AppLanguage();
+  await appLanguage.fetchLocale();
+  runApp(MyApp(
+    appLanguage: appLanguage,
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final AppLanguage appLanguage;
+
+  MyApp({this.appLanguage});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Af.Fix Smartphone Repair - Baiki Smartphone anda dengan mudah',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      routeInformationParser: VxInformationParser(),
-      routerDelegate: VxNavigator(
-        routes: {
-          '/': (_, __) => MaterialPage(child: LandingPage()),
-          MyRoutes.myRepairID: (_, __) =>
-              MaterialPage(child: MyRepairIDDisplay()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppLanguage>(create: (context) => appLanguage)
+      ],
+      child: Consumer<AppLanguage>(
+        builder: (context, model, child) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            title:
+                'Af.Fix Smartphone Repair - Baiki Smartphone anda dengan mudah',
+            theme: ThemeData(
+              primarySwatch: Colors.red,
+            ),
+            routeInformationParser: VxInformationParser(),
+            routerDelegate: VxNavigator(
+              routes: {
+                '/': (_, __) => MaterialPage(child: LandingPage()),
+                MyRoutes.myRepairID: (_, __) =>
+                    MaterialPage(child: MyRepairIDDisplay()),
+              },
+            ),
+            locale: model.appLocal,
+            supportedLocales: [
+              Locale('my', ''),
+              Locale('en', 'US'),
+            ],
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+          );
         },
       ),
     );
