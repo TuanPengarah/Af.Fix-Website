b0f00b055f0308a64d87a8102a2908346be9be5b
Akid Fikri Azhar <akidfikriazhar@gmail.com>
2021-05-10 13:30:24 +0800

    Fix manifest error Fix PWA not showing to install on web browser
    Signed-off-by: Akid Fikri Azhar <akidfikriazhar@gmail.com>

--- lib/screen/myrepairid.dart
+++ lib/screen/myrepairid.dart
@@ -1,3 +1,4 @@
+import 'package:affix_web/config/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -13,70 +14,74 @@ class _MyRepairIDDisplayState extends State<MyRepairIDDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyRepair ID'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _inputIdController,
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.center,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (_inputIdController.text.isNotEmpty) {
-                            _search = true;
-                            _input = _inputIdController.text.toString();
-                            print(_input);
-                          }
-                        });
-                      },
-                      child: Text('Cari'),
+    return Title(
+      title: 'MyRepair ID - Af-Fix Smartphone Repair',
+      color: kColorRed,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('MyRepair ID'),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _inputIdController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Divider(),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_inputIdController.text.isNotEmpty) {
+                              _search = true;
+                              _input = _inputIdController.text.toString();
+                              print(_input);
+                            }
+                          });
+                        },
+                        child: Text('Cari'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Divider(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            _search == true
-                ? StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('MyrepairID')
-                        .doc(_input)
-                        .snapshots(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<DocumentSnapshot> snapshot) {
-                      if (!snapshot.hasData) {
-                        return Text('Loading...');
-                      } else if (!snapshot.data.exists) {
-                        return Text('Tiada data ditemui');
-                      }
-                      var userDocument = snapshot.data;
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Model: ${userDocument['Model']}'),
-                          Text('Kerosakkan: ${userDocument['Kerosakkan']}'),
-                          Text('Status: ${userDocument['Status']}'),
-                        ],
-                      );
-                    },
-                  )
-                : Container(),
-          ],
+              _search == true
+                  ? StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('MyrepairID')
+                          .doc(_input)
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (!snapshot.hasData) {
+                          return Text('Loading...');
+                        } else if (!snapshot.data.exists) {
+                          return Text('Tiada data ditemui');
+                        }
+                        var userDocument = snapshot.data;
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Model: ${userDocument['Model']}'),
+                            Text('Kerosakkan: ${userDocument['Kerosakkan']}'),
+                            Text('Status: ${userDocument['Status']}'),
+                          ],
+                        );
+                      },
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
