bd286769301773a5eb8001d16df78a51729a8095
TuanPengarah <akidfikriazhar@gmail.com>
2021-07-05 19:56:21 +0800

    Add change name and phone number Add change email address More improvement on dark mode Improve translation

---
+++ lib/model/update_user_info.dart
@@ -0,0 +1,67 @@
+import 'package:affix_web/config/app_localizations.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ndialog/ndialog.dart';
+import 'dart:html' as html;
+
+Future<void> updateUser({
+  @required BuildContext context,
+  @required String inputPhone,
+  @required String inputName,
+}) async {
+  CustomProgressDialog progressDialog = CustomProgressDialog(context, blur: 15);
+  progressDialog.setLoadingWidget(
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor),
+        ),
+        SizedBox(height: 10),
+        Text(
+          '${AppLocalizations.of(context).translate('updatinguser')}',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+  );
+  progressDialog.show();
+  print('Updating user data..');
+  print(inputName);
+  print(inputPhone);
+  User user = FirebaseAuth.instance.currentUser;
+  var firestoreCollection =
+      FirebaseFirestore.instance.collection('customer').doc(user.uid);
+  //Update Name dan Phone Number di  Firestore
+  await firestoreCollection.update({
+    'Nama': inputName,
+    'No Phone': inputPhone,
+  });
+
+  //Update User Auth Name
+  await user.updateProfile(displayName: inputName);
+  //Update Email
+  // await context.read<AuthenticationServices>().updateEmail(inputEmail);
+  // if (Provider.of<AuthenticationServices>(context, listen: false).status ==
+  //     'requires-recent-login') {
+  //   await reauthUserDialog();
+  // }
+  await user.reload().then((value) {
+    print('Updating completed');
+    progressDialog.dismiss();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      action: SnackBarAction(
+        label: 'Refresh',
+        onPressed: () {
+          html.window.location.reload();
+        },
+      ),
+      content:
+          Text('${AppLocalizations.of(context).translate('updatecomplete')}'),
+    ));
+  });
+}
